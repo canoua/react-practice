a8a3155ec00c0772afe6d73bb34ad85c74f5a707
akaryaka <a.karyaka.2dit@gmail.com>
2023-08-08 17:07:04 +0600

    add robots, manifest; refactoring: add class App

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,18 @@
+import React, {Component} from 'react';
 import Book from './../book/book';
 
-function App() {
-  return (
-    <div className="App">
-      <Book  year="2023 год" price="1000р">
-        text
-      </Book>
-      <Book name="JS" year="2023 год" price="1000р"/>
-      <Book name="React" year="2023 год" price="500р"/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Book  year="2023 год" price="1000р">
+          text
+        </Book>
+        <Book name="JS" year="2023 год" price="1000р"/>
+        <Book name="React" year="2023 год" price="500р"/>
+      </div>
+    );
+  }
 }
 
 export default App;
